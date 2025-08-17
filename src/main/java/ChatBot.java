026b4f9e1ae610f2c4820909dce05ae717d4a05b
Anxinal <ax1375240926@gmail.com>
2025-08-17 21:56:41 +0800

    Add greeting and exit

--- src/main/java/ChatBot.java
+++ src/main/java/ChatBot.java
@@ -2,7 +2,15 @@ public class ChatBot {
     static String name = "SHIROHA"; 
     static String logo = "Chatbot - Shiroha XD";
 
-
+    public static void greet(){
+        System.out.println("Hello I am your Chatbot "+ ChatBot.name);
+        System.out.println("Anything in your mind right now? Want to walk by the sea?");
+        addLineBreak();
+    }
+    public static void exit(){
+        System.out.println("See you.");
+        addLineBreak();
+    }
     public static void addLineBreak(){
         System.out.println("----------------");
     }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ public class Main {
     static String name = "SHIROHA";
         
     public static void main(String[] args) {
-
+      ChatBot.greet();
+      ChatBot.exit();
     }
 }
