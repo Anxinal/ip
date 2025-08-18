06e82ac45fdf4fc6f3904cf2d47a89725de28e49
Anxinal <ax1375240926@gmail.com>
2025-08-18 14:16:35 +0800

    Define the hierarchy of the different tasks

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -6,13 +6,12 @@ public class Task {
         String finishStat = isDone ? "X" : " ";
         return "["+ finishStat +"]" + " " + description;
     }
-
     private Task(String description){
         this.description = description;
         this.isDone = false;
     }
 
-    public static Task newTask(String description){
+    public static Task newTask(int type, String description){
         return new Task(description);
     }
 
@@ -23,5 +22,40 @@ public class Task {
     public void unmark(){
         this.isDone = false;
     }
+
+    private static class TodoTask extends Task{
+        private TodoTask(String description){
+            super(description);
+        }
+        @Override
+        public String toString(){
+            return "[T]" + super.toString();
+        }
+    }
+    private static class EventTask extends Task{
+        String from;
+        String to;
+        private EventTask(String description, String from, String to){
+            super(description);
+            this.from = from;
+            this.to = to;
+        }
+        @Override
+        public String toString(){
+            return "[E]" + super.toString() + String.format(" (from: %s to: %s)", from, to);
+        }
+    }
+    private static class DeadlineTask extends Task{
+        String deadline;
+        private DeadlineTask(String description, String deadline){
+            super(description);
+            this.deadline = deadline;
+        }
+        @Override
+        public String toString(){
+            return "[T]" + super.toString() + String.format(" (by %s)", deadline);
+        }
+    }
+
 
 }
