b1b6566562a7f029b02d081edbaceb531c339e9e
Anxinal <ax1375240926@gmail.com>
2025-09-02 12:51:05 +0800

    Initialise the main page

--- src/main/java/shiroha/ui/UI.java
+++ src/main/java/shiroha/ui/UI.java
@@ -1,10 +1,18 @@
 package shiroha.ui;
 import java.util.Scanner;
 
+import javafx.scene.Scene;
+import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 public class UI {
 
-    Scanner s = new Scanner(System.in);
-
+    private Scene mainPage;
     /**
      * Renders a message from the chatbot to the user with line breaks before and after the message
      * @param s The message to be rendered
@@ -13,6 +21,45 @@ public class UI {
         addLineBreak();
         System.out.println(s);
         addLineBreak();
+    }
+    public UI(){
+         ScrollPane scrollPane = new ScrollPane();
+         VBox dialogContainer = new VBox();
+         scrollPane.setContent(dialogContainer);
+
+         TextField userInput = new TextField();
+         Button sendButton = new Button("Send");
+
+         AnchorPane mainLayout = new AnchorPane();
+         mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+                 mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput, 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+        
+        this.mainPage = new Scene(mainLayout);
+    }
+
+    public Scene getScene(){
+        return this.mainPage;
     }
     /**
      * Renders an error message to the user with line breaks before and after the message
@@ -32,14 +79,14 @@ public class UI {
      * @return The next line of input from the user
      */
     public String getNextInput(){
-        return this.s.nextLine();
+        return "";
     }
 
     /**
      * Closes the scanner
      */
     public void close(){
-        this.s.close();
+        return;
     }
 
 }
