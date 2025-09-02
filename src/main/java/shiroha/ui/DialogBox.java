a6f1d5c6c80e7c35e09b1f79faa81be79d7a56d9
Anxinal <ax1375240926@gmail.com>
2025-09-02 16:30:44 +0800

    Reverse the user dialog box

--- src/main/java/shiroha/ui/DialogBox.java
+++ src/main/java/shiroha/ui/DialogBox.java
@@ -1,19 +1,29 @@
 package shiroha.ui;
 
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.Node;
 
 public class DialogBox extends HBox {
+
+    public enum DigalogType {
+        NORMAL,
+        USER,
+        ERROR,
+    }
 
     private Label text;
     private ImageView displayPicture;
     private static final double FIT_WIDTH = 110.0;
     private static final double FIT_HEIGHT = 110.0;
-    public DialogBox(String s, Image i) {
+
+    private DialogBox(String s, Image i) {
 
         text = new Label(s);
         displayPicture = new ImageView(i);
@@ -25,5 +35,25 @@ public class DialogBox extends HBox {
         this.getChildren().addAll(text, displayPicture);
         HBox.setMargin(displayPicture, getInsets());
     }
+
+    public static DialogBox initialiseDialogBox(String s, Image i, DigalogType type) {
+
+        var db = new DialogBox(s, i);
+        switch (type) {
+        case USER:
+            db.setAlignment(Pos.TOP_LEFT);
+            ObservableList<Node> tmp = FXCollections.observableArrayList(db.getChildren());
+            FXCollections.reverse(tmp);
+            db.getChildren().setAll(tmp);
+            break;
+        case ERROR:
+            db.setStyle("-fx-background-color: #ffcccc; -fx-border-color: red; -fx-border-width: 2px;");
+            break;
+        default:
+            break;
+        }
+        return db;
+    }
+
 
 }
