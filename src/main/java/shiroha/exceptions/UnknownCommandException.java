abfb8a7e66a72c7ea7c03a50845785046afdbfe6
Anxinal <ax1375240926@gmail.com>
2025-09-28 21:40:14 +0800

    Add more javadoc

--- src/main/java/shiroha/exceptions/UnknownCommandException.java
+++ src/main/java/shiroha/exceptions/UnknownCommandException.java
@@ -3,6 +3,10 @@ package shiroha.exceptions;
 
 public class UnknownCommandException extends RuntimeException {
     private static final String HEAD = "Emm. I don't quite get what you are saying. ";
+    /**
+     * Constructor for UnknownCommandException
+     * @param message the message to be displayed by the chatbot
+     */
     public UnknownCommandException(String message){
         super(HEAD + message);
     }
